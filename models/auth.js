4dc36a2365e37baf0da9e053fede695eca974fdf
decypher1 <mumekwe2@gmail.com>
2024-04-30 04:15:32 +0100

    VIEWS

--- models/auth.js
+++ models/auth.js
@@ -10,46 +10,5 @@ const authSchema = new Schema ({
 
 authSchema.plugin(passportLocalMongoose);
 
-// const authSchema = new Schema({
-//     firstname : {
-//         type : String,
-//         required: true
-//     },
 
-//     lastname: {
-//         type: String,
-//         require: true,
-//     },
-
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-
-//     password: {
-//         type: String,
-//         required: true
-//     },
-
-//     email: {
-//         type: String,
-//         required: true,
-//     },
-//     phone: {
-//         type: Number,
-//         required: false,
-//         min: [11, 'Must be 11 digits']
-//     },
-
-//     createAt: {
-//         type: Date,
-//         default: Date.now
-//     },
-
-//     updateAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-// })
 module.exports = mongoose.model('users', authSchema);//collection name is user in the db
